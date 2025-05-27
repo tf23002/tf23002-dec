a27f7dd51996e065cc4cd3f16474eb01c2bbfe48
tf23002 <tf23002@ues.edu.sv>
2025-05-27 19:40:00 -0400

    Corregido resta3.asm para devolver resultado como código de salida

--- resta3.asm
+++ resta3.asm
@@ -1,17 +1,9 @@
-section .data
-    num1 dw 20
-    num2 dw 5
-    num3 dw 3
-    result dw 0
-
 section .text
-    global _start
+global _start
 
 _start:
-    mov ax, [num1]   ; ax = num1 (20)
-    sub ax, [num2]   ; ax = ax - num2 (20 - 5 = 15)
-    sub ax, [num3]   ; ax = ax - num3 (15 - 3 = 12)
-    mov [result], ax ; guardar el resultado
-
-    mov eax, 1
-    int 0x80
+    mov eax, 7      ; numero 1
+    sub eax, 4      ; numero 2 (resta: 7 - 4)
+    mov ebx, eax    ; mover el resultado a ebx para el codigo de salida
+    mov eax, 1      ; syscall: sys_exit
+    int 0x80        ; ejecutar la salida con el resultado
